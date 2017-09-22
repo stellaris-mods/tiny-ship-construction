517f87de19720f62da71d3c800e968888e4ef1f9
folknor <folk@folk.wtf>
2017-09-22 13:07:27 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Tiny Ship Construction",
 	tags = { "Graphics", "Utilities" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 803553012,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
